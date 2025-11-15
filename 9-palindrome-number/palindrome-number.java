f993c6b451df0ea63aaf2fb67d0b765c8d9883ab
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2025-11-15 10:56:27 +0530

    Time: 5 ms (66.80%) | Memory: 45.9 MB (8.60%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+        int revertednumber=0;
+        while(x>revertednumber){
+            revertednumber=revertednumber*10+x%10;
+            x/=10;
+        }
+        return x==revertednumber || x==revertednumber /10;
+    }
+}
