7c941654fb10b8069551abecc0485f0b403a8a4c
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-14 15:17:24 +0530

    Time: 4 ms (98.30%) | Memory: 101.2 MB (48.21%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        // Step 1: Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2: Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode curr = slow;
+        
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        // Step 3: Calculate the maximum twin sum
+        int maxSum = 0;
+        ListNode firstHalf = head;
+        ListNode secondHalf = prev; // 'prev' is the head of the reversed second half
+        
+        while (secondHalf != null) {
+            maxSum = Math.max(maxSum, firstHalf.val + secondHalf.val);
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        
+        return maxSum;
+    }
+}
