e74a51dd2265cd54110286d8ab281fb597dc1194
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-07-31 09:58:27 +0530

    Time: 1 ms (99.84%) | Memory: 48 MB (23.05%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+        
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != 'O') {
+            return;
+        }
+        
+        board[r][c] = '*';
+        
+        dfs(board, r + 1, c);
+        dfs(board, r - 1, c);
+        dfs(board, r, c + 1);
+        dfs(board, r, c - 1);
+    }
+}
