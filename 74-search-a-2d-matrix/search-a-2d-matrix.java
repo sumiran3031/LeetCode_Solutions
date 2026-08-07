3b15bbddd19c23c95f11b269cdd82e5587d489c2
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-08-07 07:49:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.91%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = (m * n) - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midVal = matrix[mid / n][mid % n];
+            
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
