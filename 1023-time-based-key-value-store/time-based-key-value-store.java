962509733789697b0f53bcbf408527c481df9a05
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-08-11 08:36:02 +0530

    Time: 136 ms (60.50%) | Memory: 107.6 MB (97.55%) - LeetSync

---
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+class TimeMap {
+
+    private static class Data {
+        String value;
+        int timestamp;
+
+        Data(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    private HashMap<String, List<Data>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) {
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(new Data(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) {
+            return "";
+        }
+        
+        List<Data> list = map.get(key);
+        int start = 0;
+        int end = list.size() - 1;
+        String res = "";
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (list.get(mid).timestamp <= timestamp) {
+                res = list.get(mid).value;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return res;
+    }
+}
