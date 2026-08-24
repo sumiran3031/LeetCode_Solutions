d31397341b5eca16abcf0b1d098a131d71083aec
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-08-24 23:29:35 +0530

    Time: 1 ms (60.96%) | Memory: 42.2 MB (84.97%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            ans |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return ans;
+    }
+}
